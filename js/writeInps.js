df187f7a97c7d68ab3022eac94cc56936dc3127b
chrisnorth <chris.north@astro.cf.ac.uk>
2014-06-25 12:57:20 +0100

    does search on lookUP

--- js/writeInps.js
+++ js/writeInps.js
@@ -1,7 +1,32 @@
 // Jquery based stuff
+    
+function parseResults(jData){
+    console.log(jData);
+}
+
 $(document).ready(function(){
     $("#object").change(function(){
 	var obj= $("#object").val();
 	//var simbadLink=
 	$('body').append(obj);
-    });
+    })
+    
+    function getResults(url,callback){
+	var form = document.forms[0];
+	var object = form['name'].value
+	// Encode and escape single quotes
+	var obj = encodeURIComponent(object).replace(/\'/,"\\\'");
+	if(object){
+	    var headID = document.getElementsByTagName("head")[0];
+	    var newScript = document.createElement('script');
+	    newScript.type = 'text/javascript';
+	    newScript.src = url+''+obj+'&callback='+callback;
+	    headID.appendChild(newScript);
+	}
+    }
+
+    console.log('searching');
+    getResults('http://strudel.org.uk/lookUP/json/?callback=lk&name=','parseResults');
+    
+});
+
