0176203a5bb3ceff0fb55b025393d8499cefe664
chrisnorth <chris.north@astro.cf.ac.uk>
2014-06-25 13:29:14 +0100

    now it works!

--- js/writeInps.js
+++ js/writeInps.js
@@ -23,6 +23,7 @@ function getResults(){
     }
 }
 
+
 $(document).ready(function(){
     $("#object").change(function(){
 	var obj= $("#object").val();
@@ -30,7 +31,36 @@ $(document).ready(function(){
 	$('body').append(obj);
     })
     
+    var QueryString = function () {
+	// This function is anonymous, is executed immediately and 
+	// the return value is assigned to QueryString!
+	var query_string = {};
+	var query = window.location.search.substring(1);
+	var vars = query.split("&");
+	for (var i=0;i<vars.length;i++) {
+	    var pair = vars[i].split("=");
+    	    // If first entry with this name
+	    if (typeof query_string[pair[0]] === "undefined") {
+		query_string[pair[0]] = pair[1];
+    		// If second entry with this name
+	    } else if (typeof query_string[pair[0]] === "string") {
+		var arr = [ query_string[pair[0]], pair[1] ];
+		query_string[pair[0]] = arr;
+    		// If third or later entry with this name
+	    } else {
+		query_string[pair[0]].push(pair[1]);
+	    }
+	} 
+	return query_string;
+    } ();
 
+    inpName = QueryString.name;
+    console.log(inpName);
+
+    if (inpName){
+	console.log('using object',inpName);
+	$('#object').val(inpName);
+    }
     //console.log('searching');
     //getResults();
     
