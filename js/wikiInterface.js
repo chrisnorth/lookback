f982530a1ebaacb4d3459956a5f7496f643f119b
Adam Avison <aavison@Adam-Avisons-MacBook-Pro.local>
2014-06-27 16:05:12 +0100

    Made wikiInterface separate distance and unit correctly for use in look back time calc

--- js/wikiInterface.js
+++ js/wikiInterface.js
@@ -3,10 +3,12 @@
 function wikiInterface(){
 
     simbadResult=$("#distancevalue").html();
-    var dists = simbadResult.split("\\s{2,}");
-    console.log(dists);
-
-    earthTime=lookBackTime(); //will need values to pass eventually
+    var dists = simbadResult.replace(/\s+/g, ' ');//replaces multiple whitespace with single
+    dists = dists.trim();//trims leading and trailing whitespace
+    var dista = dists.split(" ");//then split on whitespace
+    console.log(dista[0]);
+    
+    earthTime=lookBackTime(dista[0],dista[1]); //will need values to pass eventually
 
     var earthYear=earthTime[0];
     var earthEpoch=earthTime[1];
